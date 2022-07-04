e458c542e8248498e0f255b8961947888b1de54a
BonnW <pwonghansa@gmail.com>
2022-07-04 17:02:28 -0500

    basic MVP nearly complete. MUI successfully implemented. Next task, Popover

--- main/src/components/Sidebar/Sidebar.js
+++ main/src/components/Sidebar/Sidebar.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
 import CssBaseline from '@mui/material/CssBaseline';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
@@ -12,19 +11,21 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Avatar from '@mui/material/Avatar';
+import { Typography, Link } from '@mui/material';
+
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
-
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 import pic from '../../assets/SampleProfilePic.jpg';
 import lofi from '../../assets/lofiWallpaper.jpg';
 import { height } from '@mui/system';
 
-const sidebarWidth = 240;
+const sidebarWidth = 260;
 
 class Sidebar extends React.Component {
-  
-
   render() {
     const { classes, childern } = this.props;
 
@@ -35,9 +36,6 @@ class Sidebar extends React.Component {
           position='fixed'
           sx={{ width: `calc(100% - ${sidebarWidth}px)`, ml: `${sidebarWidth}`}}  
         >
-          <Toolbar>
-            <p>hello world</p>
-          </Toolbar>
         </AppBar>
         <Drawer
           sx={{
@@ -52,17 +50,68 @@ class Sidebar extends React.Component {
           variant='permanent'
           anchor='left'
         >
-          <Toolbar 
-            // display='flex'
-            // justifyContent='center'
-            // alignItems='center'
+          {/* START AVATAR */}
+          <Box
+            sx={{
+              paddingTop: 3,
+              paddingBottom: 2,
+              
+            }}
           >
             <Avatar
               alt='goat'
               src={pic}
-              sx={{ width: 100, height: 100}}
+              sx={{ 
+                border: '2px solid black',
+                width: 180, 
+                height: 180,
+                t: 10,
+                m: 'auto',
+              }}
             />
-          </Toolbar>
+            {/* END AVATAR */}
+            {/* START HEADER */}
+            <Typography
+              variant='h4'
+              sx={{
+                paddingTop: 1,
+                fontWeight: 'bold',
+                fontSize: 22,
+                color: 'white',
+              }}
+            >
+              Bonn W.
+            </Typography>
+            <Typography
+              variant='h5'
+              sx={{
+                fontWeight: 'bold',
+                fontSize: 22,
+                color: 'white',
+                pb: 2
+              }}
+            >
+              Full-Stack Developer
+            </Typography>
+            {/* END HEADER */}
+            {/* START SOCIAL MEDIA ICONS */}
+            <Box
+              sx={{
+                mx: 2,
+                px: 3,
+                pt: 0.6,
+                display: 'flex',
+                justifyContent: 'space-around',
+                backgroundColor: 'white',
+                border: '2px solid black'
+              }}
+            >
+              <Link color='inherit' target='_blank' href='https://github.com/BonnW'><GitHubIcon sx={{ fontSize: 40}}/></Link>
+              <Link color='inherit' target='_blank' href='https://www.linkedin.com/in/bonn-wonghansa/'><LinkedInIcon sx={{ fontSize: 40}}/></Link>
+              <Link color='inherit' target='_blank' href='https://twitter.com/heyitsbonn'><TwitterIcon sx={{ fontSize: 40}}/></Link>
+            </Box>
+            {/* END SOCIAL MEDIA ICONS */}
+          </Box>
           <Divider />
           <List>
           {['About Me', 'Projects'].map((text, index) => (
@@ -71,35 +120,24 @@ class Sidebar extends React.Component {
                 <ListItemIcon>
                   {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
                 </ListItemIcon>
-                <ListItemText primary={text} />
+                <ListItemText primary={text} sx={{ color: 'white', }} primaryTypographyProps={{ fontSize: 20 }} />
               </ListItemButton>
             </ListItem>
           ))}
         </List>
         </Drawer>
-        <Container
+        <Box
           component='main'
           sx={{
             flexGrow: 1,
-            backgroundImage: {lofi},
-            alt: 'background',
-            backgroundColor: 'cyan',
-            height: 500,
+            backgroundImage: `url(${lofi})`,
+            backgroundColor: 'green',
+            height: '100vh',
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
           }}
         >
-
-        </Container>
-        {/* <Box
-          component='main'
-          sx={{
-            flexGrow: 1,
-            backgroundImage: {lofi},
-            alt: 'background',
-            // backgroundColor: 'cyan',
-            height: 500,
-          }}
-        >
-        </Box> */}
+        </Box>
       </Box>
     )
   }
