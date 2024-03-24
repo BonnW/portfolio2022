46cffdd4f6fed5850cd06f42f00b9ad1193b09ad
Bonn W <pwonghansa@gmail.com>
2024-03-24 18:55:17 -0500

    added ytplayer

--- main/src/components/Wallpaper.js
+++ main/src/components/Wallpaper.js
@@ -1,25 +1,31 @@
-import React, { Component } from "react";
-import Box from "@mui/material/Box";
+import React, { Component, useEffect } from 'react';
+import Box from '@mui/material/Box';
 
-import lofi from "../assets/lofiWallpaper.jpg";
+import lofi from '../assets/lofiWallpaper.jpg';
 
 // NEXT STEPS
 // STREAM MINI YOUTUBE PLAYER IN BACKGROUND.
 // VISITORS ON ARRIVAL, VIDEO PAUSED. CAN PLAY AT WILL. AND/AOR MINIMIZE
 
+let player;
+
 class Wallpaper extends Component {
   render() {
     return (
       <Box
-        component="main"
+        component='main'
         sx={{
-          ml: "260px",
+          ml: '260px',
           flexGrow: 1,
           backgroundImage: `url(${lofi})`,
-          backgroundColor: "green",
-          height: "100vh",
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
+          backgroundColor: 'green',
+          height: '100vh',
+          width: '100vw',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          display: 'flex',
+          overflowX: 'hidden',
         }}
       ></Box>
     );
