e00562c4ddf28ce446f53cde80e484988bad75e4
pbwonghansa@copar.com <pwonghansa@gmail.com>
2024-03-24 20:06:12 -0500

    working yt player

--- main/src/components/Wallpaper.js
+++ main/src/components/Wallpaper.js
@@ -1,35 +1,94 @@
-import React, { Component, useEffect } from 'react';
-import Box from '@mui/material/Box';
+import React, { Component, useEffect } from "react";
+import Box from "@mui/material/Box";
+import { styled } from "@mui/system";
 
-import lofi from '../assets/lofiWallpaper.jpg';
+import lofi from "../assets/lofiWallpaper.jpg";
 
 // NEXT STEPS
 // STREAM MINI YOUTUBE PLAYER IN BACKGROUND.
 // VISITORS ON ARRIVAL, VIDEO PAUSED. CAN PLAY AT WILL. AND/AOR MINIMIZE
 
-let player;
+const sidebarWidth = 260;
 
-class Wallpaper extends Component {
-  render() {
-    return (
-      <Box
-        component='main'
-        sx={{
-          ml: '260px',
-          flexGrow: 1,
-          backgroundImage: `url(${lofi})`,
-          backgroundColor: 'green',
-          height: '100vh',
-          width: '100vw',
-          backgroundRepeat: 'no-repeat',
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          display: 'flex',
-          overflowX: 'hidden',
-        }}
-      ></Box>
-    );
-  }
+const StyledBox = styled("div")({
+  flexGrow: 1,
+  backgroundImage: `url(${lofi})`,
+  backgroundColor: "green",
+  height: "100vh",
+  width: `calc(100vw - ${sidebarWidth}px)`,
+  marginLeft: `${sidebarWidth}px`,
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  display: "flex",
+  overflowX: "hidden",
+  justifyContent: "center",
+  alignItems: "center",
+});
+
+const YouTubePlayer = ({ videoId }) => {
+  let player;
+
+  useEffect(() => {
+    // init youtube player when component mounts
+    const script = document.createElement("script");
+    script.src = "https://www.youtube.com/iframe_api";
+    document.body.appendChild(script);
+
+    window.onYouTubeIframeAPIReady = initilizePlayer;
+
+    // Cleanup function to remove the youtube player when component unmounts
+    return () => {
+      if (player) {
+        player.destroy();
+      }
+    };
+  }, []);
+
+  const initilizePlayer = () => {
+    console.log("initilizing yt player");
+    player = new window.YT.Player("youtube-player", {
+      height: "390",
+      width: "640",
+      videoId: videoId,
+      playerVars: {
+        autoplay: 1,
+        controls: 1,
+        rel: 0,
+      },
+      events: {
+        onReady: onPlayerReady,
+        onStateChange: onPlayerStateChange,
+      },
+    });
+  };
+
+  const onPlayerReady = (event) => {
+    console.log("player ready event triggered");
+    event.target.playVideo();
+  };
+
+  const onPlayerStateChange = (event) => {
+    console.log("player state change event triggered", event.data);
+  };
+
+  return (
+    <div
+      id="youtube-player"
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100%",
+      }}
+    ></div>
+  );
+};
+
+export default function Wallpaper() {
+  return (
+    <StyledBox>
+      <YouTubePlayer videoId="7NOSDKb0HlU" />
+    </StyledBox>
+  );
 }
-
-export default Wallpaper;
